8dd5f40aa73f1dad02c1ece6d0c76d609f516543
Poornima2003 <107053451+Poornima2003@users.noreply.github.com>
2024-06-05 00:22:53 +0000

    contextapi

---
+++ 08_miniContext/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+import UserContext from './Context/UserContext'
+import UserContextProvider from './Context/UserContextProvider'
+
+function App() {
+  
+  return (
+    <UserContextProvider >
+     <h1>poornima theurkar</h1>
+    <Login/>
+    <Profile/>
+   </UserContextProvider>
+  )
+}
+
+export default App
